ab37bb0bc9eca7c90149147316aa7b6ad691eb32
mek12 <49874601+mek12@users.noreply.github.com>
2019-12-27 19:10:47 +0300

    Add files via upload

---
+++ multiple-linear-regression.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 27 13:14:24 2019
+
+@author: Emir
+"""
+
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+df=pd.read_csv("multiple-linear-regression-dataset.csv", sep=";")
+
+x=df.iloc[:,[0,2]].values
+y=df.maas.values.reshape(-1,1)
+
+multiple_linear_regression=LinearRegression()
+a=multiple_linear_regression.fit(x,y)
+
+print("b0: ", multiple_linear_regression.intercept_)
+print("b1,b2: ", multiple_linear_regression.coef_)
+ 
+multiple_linear_regression.predict(np.array([[10,35],[5,35]]))
